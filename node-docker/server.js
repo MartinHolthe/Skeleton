783f9b12124d1a3fcc86930763ee50abb12c8fa7
Martin Holthe <martinholthe@hotmail.no>
2022-04-13 16:07:00 +0200

    added .env

--- node-docker/server.js
+++ node-docker/server.js
@@ -1,6 +1,6 @@
-const express = require("express");
-var cors = require("cors");
-const pool = require("./db");
+const express = require('express');
+var cors = require('cors');
+const pool = require('./db');
 const app = express();
 
 //var corsOption = { origin: "http://localhost:9000" };
@@ -10,7 +10,7 @@ app.use(express.json()); // => req.body
 
 //default to port 8080 if env is not set
 const PORT = 8080;
-const HOST = "0.0.0.0";
+const HOST = '0.0.0.0';
 //var corsOption = { origin: "http://localhost:9000" };
 
 //app.use(cors(corsOption)); // this need to be configured according to CORS
@@ -18,22 +18,22 @@ app.use(express.json()); // => req.body
 
 // App
 
-app.get("/", async (req, res) => {
-  res.status(200).json({ message: "hello Docker nodemon" });
+app.get('/', async (req, res) => {
+	res.status(200).json({ message: 'hello Docker' });
 });
 
 //GET all rows from table
-app.get("/accounts", async (req, res) => {
-  try {
-    const allBundesliga = await pool.query("SELECT * FROM accounts");
+app.get('/accounts', async (req, res) => {
+	try {
+		const allBundesliga = await pool.query('SELECT * FROM accounts');
 
-    res.setHeader("Content-Type", "application/json");
-    res.json(allBundesliga.rows);
-  } catch (err) {
-    console.error(err.message);
-  }
+		res.setHeader('Content-Type', 'application/json');
+		res.json(allBundesliga.rows);
+	} catch (err) {
+		console.error(err.message);
+	}
 });
 
 app.listen(PORT, HOST, () => {
-  console.log(`Running on http://localhost:${PORT}`);
+	console.log(`Running on http://localhost:${PORT}`);
 });
